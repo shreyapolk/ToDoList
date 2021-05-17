ad9ee2cedd86a1bcdb939eea9417f73caf452470
Shreya Polkampally <shreyapolk@gmail.com>
2021-05-17 13:03:26 -0400

    # To Do List App (2021!)

---
+++ ToDoList/CompleteToDoViewController.swift
@@ -0,0 +1,47 @@
+//
+//  CompleteToDoViewController.swift
+//  ToDoList
+//
+//  Created by Shreya Polk on 5/17/21.
+//
+
+import UIKit
+
+class CompleteToDoViewController: UIViewController {
+
+    var previousVC = ToDoTableViewController ()
+    var selectedToDo : ToDoCD?
+    
+    @IBOutlet weak var titleLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        titleLabel.text = selectedToDo?.name 
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func completeTapped(_ sender: Any) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let context = appDelegate.persistentContainer.viewContext
+        
+        if let theToDo = selectedToDo {
+            context.delete(theToDo)
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Shreya Polk on 5/16/21.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
